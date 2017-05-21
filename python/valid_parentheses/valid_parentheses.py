16a8241c5ae7927ee8ce3fd66b804a00fd438466
yang luo <ltytuotuo@gmail.com>
2017-05-21 23:55:46 +0800

    valid-parentheses

--- python/valid_parentheses/valid_parentheses.py
+++ python/valid_parentheses/valid_parentheses.py
@@ -4,6 +4,37 @@
 import unittest
 
 
+class Solution(object):
+    def isValid(self, s):
+        """
+        :type s: str
+        :rtype: bool
+        """
+        return self.stack(s)
+
+    def stack(self, s):
+        couple, get = {"(": ")", "{": "}", "[": "]"}, []
+        for i in s:
+            if i in couple:
+                get.append(i)
+            else:
+                if not get or couple[get.pop()] != i:
+                    return False
+        return False if get else True
+
+    def couple_info(self, s):
+        if (len(s) & 1) == 1:
+            return False
+        couple, get = {"(": ")", "{": "}", "[": "]"}, []
+        for i in s:
+            if i in couple:
+                get.append(i)
+            else:
+                if not get or couple[get.pop()] != i:
+                    return False
+        return False if get else True
+
+
 class TestCase(unittest.TestCase):
     def setUp(self):
         pass
@@ -11,6 +42,9 @@ class TestCase(unittest.TestCase):
     def tearDown(self):
         pass
 
+    def test_check(self):
+        self.assertEqual(Solution().isValid("()[]{}"), True)
+        self.assertEqual(Solution().isValid("(()[]{}"), False)
 
 if __name__ == '__main__':
     unittest.main()
