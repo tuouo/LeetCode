2ab5d815607e0d01f6ee46243a43342ea1f063f5
yang luo <ltytuotuo@gmail.com>
2017-06-03 14:19:56 +0800

    add ListNodeDouble, LinkedListDouble, LRUCache

--- python/list_node/list_node.py
+++ python/list_node/list_node.py
@@ -52,6 +52,100 @@ class ListNode(object):
         return self.val >= other.val
 
 
+class ListNodeDouble(object):
+    def __init__(self, key, val, next_node=None, pre_node=None):
+        self.key = key
+        self.val = val
+        self.next = next_node
+        self.pre = pre_node
+
+
+class LinkedListDouble(object):
+    def __init__(self):
+        self.head = self.tail = None
+
+    def insert(self, node):
+        if self.head is None:
+            self.head = self.tail = node
+        else:
+            self.head.pre, node.next = node, self.head
+            self.head = node
+
+    def delete(self, node):
+        if node.pre:
+            node.pre.next = node.next
+        else:
+            self.head = node.next
+        if node.next:
+            node.next.pre = node.pre
+        else:
+            self.tail = node.pre
+        del node
+
+    def short(self):
+        if self.head == self.tail:
+            self.head = self.tail = None
+        else:
+            self.tail = self.tail.pre
+            del self.tail.next
+            self.tail.next = None
+
+    def top(self, node, val=None):
+        if self.head != node:
+            if self.tail == node:
+                self.tail = node.pre
+                self.head.pre, node.next, node.pre, self.tail.next = node, self.head, None, None
+                self.head = node
+            else:
+                node.pre.next, node.next.pre = node.next, node.pre
+                self.head.pre, node.next, node.pre = node, self.head, None
+                self.head = node
+        if val:
+            node.val = val
+
+
+class LRUCache(object):
+    def __init__(self, capacity):
+        """
+        :type capacity: int
+        """
+        self.capacity = capacity
+        self.dict = {}
+        self.list = LinkedListDouble()
+
+    def _insert(self, key, val):
+        node = ListNodeDouble(key, val)
+        self.list.insert(node)
+        self.dict[key] = node
+
+    def get(self, key):
+        """
+        :type key: int
+        :rtype: int
+        """
+        if key not in self.dict:
+            return -1
+        node = self.dict[key]
+        self.list.top(node)
+        return node.val
+
+    def put(self, key, value):
+        """
+        :type key: int
+        :type value: int
+        :rtype: void
+        """
+        if key in self.dict:
+            node = self.dict[key]
+            self.list.top(node, value)
+        else:
+            if len(self.dict) == self.capacity:
+                del self.dict[self.list.tail.key]
+                self.list.short()
+            self.list.insert(ListNodeDouble(key, value))
+            self.dict[key] = self.list.head
+
+
 if __name__ == '__main__':
     a = ListNode(0)
     a.next = ListNode(1)
