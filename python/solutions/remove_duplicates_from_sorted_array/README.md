95a1319c9805815de147b791df60f89fb5696e36
yang luo <ltytuotuo@gmail.com>
2017-06-06 11:21:43 +0800

    find new item to suit suitable place

---
+++ python/solutions/remove_duplicates_from_sorted_array/README.md
@@ -0,0 +1,12 @@
+https://leetcode.com/problems/remove-duplicates-from-sorted-array
+
+Given a sorted array, remove the duplicates in place such that each element appear only once and return the new length.
+Do not allocate extra space for another array, you must do this in place with constant memory. 
+For example,
+Given input array nums = [1,1,2], 
+Your function should return length = 2, with the first two elements of nums being 1 and 2 respectively. It doesn't matter what you leave beyond the new length. 
+
+############################################################
+
+给定的排好序的数组中含有重复项，要求的数组没有重复项。
+直接把后一个不重复的数（与当前处理好的数的最后一位不同）放到当前处理好的数的后一位即可。
---
+++ python/solutions/remove_duplicates_from_sorted_array/remove_duplicates_from_sorted_array.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# 2017/6/6
+import unittest
+
+
+class Solution(object):
+    def removeDuplicates(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        if not nums:
+            return 0
+        check = 0
+        for i in range(1, len(nums)):
+            if nums[check] != nums[i]:
+                check += 1
+                nums[check] = nums[i]
+        return check + 1
+
+
+class TestCase(unittest.TestCase):
+    def setUp(self):
+        pass
+
+    def tearDown(self):
+        pass
+
+    def testRemove(self):
+        origin = [1, 2, 2, 3, 4, 5, 5, 5, 6, 7]
+        self.assertEqual((Solution().removeDuplicates(origin)), 7)
+
+if __name__ == '__main__':
+    unittest.main()
