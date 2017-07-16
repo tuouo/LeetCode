bd43e7a757eab77192b73daec37d921d075ed87a
yang luo <ltytuotuo@gmail.com>
2017-07-16 23:41:58 +0800

    temp arr is a good way

---
+++ python/solutions/n_queens/README.md
@@ -0,0 +1,28 @@
+https://leetcode.com/problems/valid-sudoku
+
+The n-queens puzzle is the problem of placing n queens on an n×n chessboard such that no two queens attack each other.The n-queens puzzle is the problem of placing n queens on an n×n chessboard such that no two queens attack each other.  
+　　　　&emsp;&emsp;![](8-queens.png)    
+Each solution contains a distinct board configuration of the n-queens' placement, where 'Q' and '.' both indicate a queen and an empty space respectively.  
+
+For example,  
+There exist two distinct solutions to the 4-queens puzzle:  
+[  
+&nbsp;[".Q..",  // Solution 1  
+&nbsp; "...Q",  
+&nbsp; "Q...",    
+&nbsp; "..Q."],  
+
+&nbsp;["..Q.",  // Solution 2  
+&nbsp; "Q...",  
+&nbsp; "...Q",  
+&nbsp; ".Q.."]  
+]  
+
+############################################################
+
+这里有四个限制性条件，每个棋子的横竖斜（包括正向反向）都只能只有一个棋子。  
+
+通辅助数组记录已占用的位置信息，简单计算得出可选的位置。
+对于每一行，选择一个可行位置作为一种解法。（通过竖向（n个位置）与两个斜向（2×n-1个位置，可适当增加）的限制确定）
+
+每个解法可以只保存对应行的位置信息即可。在最后再生成图形化信息。  
---
+++ python/solutions/n_queens/n_queens.py
@@ -0,0 +1,43 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# 2017/7/16
+import unittest
+
+
+class Solution(object):
+    def solveNQueens(self, n):
+        """
+        :type n: int
+        :rtype: List[List[str]]
+        """
+        self.n, self.result = n, []
+        # Tags show no queen(empty) at postion of col, bias, bias_anti
+        self.col, self.bias, self.bias_anti = [True] * n, [True] * (2 * n), [True] * (2 * n)
+        self.solve(0, [0] * n)
+        return self.result
+
+    def solve(self, row, one):
+        if row == self.n:
+            self.result.append(list(map(lambda x: "." * x + "Q" + "." * (self.n - 1 - x), one)))
+        else:
+            for pos in range(self.n):
+                if self.col[pos] and self.bias[pos + row] and self.bias_anti[self.n - pos + row]:
+                    self.col[pos] = self.bias[pos + row] = self.bias_anti[self.n - pos + row] = False
+                    one[row] = pos  # change 'one' after we deal with row & tags, doesn't matter what's initialized
+                    self.solve(row + 1, one)
+                    self.col[pos] = self.bias[pos + row] = self.bias_anti[self.n - pos + row] = True
+
+
+class TestCase(unittest.TestCase):
+    def setUp(self):
+        pass
+
+    def tearDown(self):
+        pass
+
+    def test_gen(self):
+        self.assertEqual(Solution().solveNQueens(4), [[".Q..", "...Q", "Q...", "..Q."], ["..Q.", "Q...", "...Q", ".Q.."]])
+
+
+if __name__ == '__main__':
+    unittest.main()
