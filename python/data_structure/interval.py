c4aeb5e69deeb7d7ff4ff9c397379179f6172c22
yang luo <ltytuotuo@gmail.com>
2017-07-27 00:12:53 +0800

    str()

--- python/data_structure/interval.py
+++ python/data_structure/interval.py
@@ -8,6 +8,11 @@ class Interval(object):
         self.start = s
         self.end = e
 
+    def __str__(self):
+        return str([self.start, self.end])
+
+    __repr__ = __str__
 
 if __name__ == '__main__':
-    pass
+    i = Interval(3, 5)
+    print(i)
