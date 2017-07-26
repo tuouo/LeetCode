0fe70a2ca5a16984b99965c7d54eb48ad3d22e45
yang luo <ltytuotuo@gmail.com>
2017-07-26 23:52:08 +0800

    interval

---
+++ python/data_structure/interval.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# 2017/7/26
+
+
+class Interval(object):
+    def __init__(self, s=0, e=0):
+        self.start = s
+        self.end = e
+
+
+if __name__ == '__main__':
+    pass
