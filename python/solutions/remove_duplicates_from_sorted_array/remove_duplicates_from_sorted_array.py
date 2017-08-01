569530dace6c0e1887a0c47cdc43bee182e06b39
Tengyang Luo <ltytuotuo@gmail.com>
2017-08-01 23:54:53 +0800

    Update remove_duplicates_from_sorted_array.py

--- python/solutions/remove_duplicates_from_sorted_array/remove_duplicates_from_sorted_array.py
+++ python/solutions/remove_duplicates_from_sorted_array/remove_duplicates_from_sorted_array.py
@@ -12,6 +12,22 @@ class Solution(object):
         """
         if not nums:
             return 0
+        check, sentry = 0, nums[0]
+        for i in range(1, len(nums)):
+            if sentry != nums[i]:
+                check += 1
+                if check != i:
+                    nums[check] = nums[i]
+                sentry = nums[check]
+        return check + 1
+    
+    def removeDuplicates_old(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        if not nums:
+            return 0
         check = 0
         for i in range(1, len(nums)):
             if nums[check] != nums[i]:
